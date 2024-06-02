3eb04a60ec8bbfa15868b35ae78922c30926d55b
creih <iherr.9@gmail.com>
2024-06-02 19:19:20 +0000

    task 3 done n rready and changed dest of .js files to scripts inside static folder

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    let selectedAmenities = {};
+
+    $('.amenities input[type="checkbox"]').change(function() {
+        if ($(this).is(':checked')) {
+            selectedAmenities[$(this).data('id')] = $(this).data('name');
+        } else {
+            delete selectedAmenities[$(this).data('id')];
+        }
+        let amenitiesList = Object.values(selectedAmenities).join(', ');
+        $('.amenities h4').text(amenitiesList);
+    });
+
+    $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+        if (data.status === 'OK') {
+            $('#api_status').addClass('available');
+        } else {
+            $('#api_status').removeClass('available');
+        }
+    });
+});
